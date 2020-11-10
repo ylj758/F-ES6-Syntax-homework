f87cb93d6b95e12da67729c13b6a10b2dea4993b
yulvjun <lvjun.yu@yulvjundeMacBook-Pro.local>
2020-11-10 08:08:41 +0800

    [ylj]: implement practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
-export { parseData };
+const parseData = input => {
+    let {data, column} = input;
+    let output = data.map(person => {
+        return Object.assign({}, ...(person.map((value, i) => {
+            return {[column[i].name]: value}
+        })))
+    });
+    return output;
+}
+
+export {parseData};
